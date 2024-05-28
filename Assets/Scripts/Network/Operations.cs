ae1f27cfb0c6fa55c5a1239a8fb17faa7fd67659
jvarelaaloisio <@>
2024-05-27 22:12:39 -0300

    added checksum operations and flags. Operations are still not done tho

---
+++ Assets/Scripts/Network/Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Network
+{
+    public static class ChecksumOperationsProvider
+    {
+        private delegate void Operation(ref ulong total, byte currentByte);
+
+        private static Operation[] _operations1 = { Add, Subtract, BitShiftLeftBy3, BitShiftRightBy3, };
+        private static Operation[] _operations2 = { BitShiftRightBy3, Add, BitShiftLeftBy3, Subtract, };
+        private static void Add(ref ulong total, byte currentByte) => total += currentByte;
+        private static void Subtract(ref ulong total, byte currentByte) => total -= currentByte;
+        private static void BitShiftRightBy3(ref ulong total, byte currentByte) => total >>= 3;
+        private static void BitShiftLeftBy3(ref ulong total, byte currentByte) => total <<= 3;
+
+        public static bool IsValid(Span<Byte> bytes)
+        {
+            //TODO: Comparison between operations and lastBytes.
+            //Halp
+            return false;
+        }
+    }
+}
